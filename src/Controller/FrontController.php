7ce47fe33827b873a97669500ec6b8f2636ca17b
Sophie Verger <sophie@kernl.fr>
2022-09-06 15:53:34 +0200

    add symfony project ecommerce

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/front", name="frontName")
+     */
+    public function front():Response
+    {
+        $prenomController = 'Sophie';
+
+        // permet de retourner la vue associé à la fonction
+        return $this->render('front/front.html.twig', [
+            'prenomTwig' => $prenomController,
+        ]);
+    }
+
+
+
+
+}
