cff0c7c8d1ed2ee9ae5fcd7f2f80b9b45672ab4f
Sophie Verger <sophie@kernl.fr>
2022-09-07 14:36:10 +0200

    ajout du CRUD de adminProduit

--- src/Controller/AdminProduitController.php
+++ src/Controller/AdminProduitController.php
@@ -15,4 +15,30 @@ class AdminProduitController extends AbstractController
     {
         return $this->render('admin_produit/produit_afficher.html.twig');
     }
+
+    /**
+     * @Route("/admin/produit/ajouter", name="produit_ajouter")
+     */
+    public function produitAjouter(): Response
+    {
+        return $this->render('admin_produit/produit_ajouter.html.twig');
+    }
+
+    /**
+     * @Route("/admin/produit/modifier", name="produit_modifier")
+     */
+    public function produitModifier(): Response
+    {
+        return $this->render('admin_produit/produit_modifier.html.twig');
+    }
+
+    /**
+     * @Route("/admin/produit/supprimer", name="produit_supprimer")
+     */
+    public function produitSupprimer(): Response
+    {
+        return $this->render('admin_produit/produit_supprimer.html.twig');
+    }
+
+
 }
