929abf4869c378ef789ab38d387c18b04ff53d0a
Sophie Verger <sophie@kernl.fr>
2022-09-08 11:15:01 +0200

    ajout des paramètres des champs du formulaire

--- src/Form/ProduitType.php
+++ src/Form/ProduitType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +17,63 @@ class ProduitType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('prix')
-            ->add('description')
+            ->add('titre', TextareaType::class, [
+                "label" => 'Nom du produit *',
+                'attr' => [
+                    "placeholder" => "saisissez le nom du produit",
+                    "class" => "border border-danger border-2 rounded"
+                ],
+                'help' => '5 caractères minimum',
+                'label_attr' => [
+                    'id' => 'label_titre',
+                    'class' => 'text-success'
+                ],
+                'help_attr' => [
+                    'class' => 'text-warning'
+                ],
+                'row_attr' => [
+                    'class' => 'shadow p-2'
+                ],
+            ])
+            ->add('prix', MoneyType::class, [
+                'currency' => 'USD',
+                "label" => 'Prix du produit *',
+                'attr' => [
+                    "placeholder" => "saisissez le nom du produit",
+                    "class" => "border border-info border-2 rounded"
+                ],
+                'help' => 'en euro',
+                'label_attr' => [
+                    'class' => 'text-info'
+                ],
+                'help_attr' => [
+                    'class' => 'text-warning'
+                ],
+                'row_attr' => [
+                    'class' => 'shadow p-2'
+                ],
+            ])
+            ->add('description', TextType::class,[
+                'required' => false,
+                "label" => 'Description du produit (facultatif)',
+                'attr' => [
+                    "placeholder" => "saisissez la description du produit",
+                    "class" => "border border-warning border-2 rounded",
+                    'rows' => 5
+                ],
+                'help' => '200 caractères minimum',
+                'label_attr' => [
+                    'class' => 'text-info'
+                ],
+                'help_attr' => [
+                    'class' => 'text-success'
+                ],
+                'row_attr' => [
+                    'class' => 'shadow p-2'
+                ],
+            ])
             ->add('image')
+            ->add('Ajouter', SubmitType::class)
         ;
     }
 
