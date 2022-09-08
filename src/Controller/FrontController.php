9de795b6bffd8714a47ab19b70e3e9aec7045972
Sophie Verger <sophie@kernl.fr>
2022-09-08 17:00:03 +0200

    ajout du catalogue et de la fiche produit

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,7 +31,27 @@ class FrontController extends AbstractController
         return $this->render('front/home.html.twig');
     }
 
+    /**
+     * @Route("/catalogue", name="catalogue")
+     */
+    public function catalogue(ProduitRepository $repoProduit):Response
+    {
+        $produits = $repoProduit->findAll();
+        return $this->render('front/catalogue.html.twig', [
+            'produits' => $produits,
+        ]);
+    }
 
+
+    /**
+     * @Route("/fiche_produit/{id}", name="fiche_produit")
+     */
+    public function fiche_produit(Produit $produit):Response
+    {
+        return $this->render('front/fiche_produit.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
 
 
 }
